b93a07239c785dd2ea18e12ca3eb0a9c5ddd69e8
jmtome <jtome@fi.uba.ar>
2025-06-12 23:06:13 -0300

    "Add requirements.txt"

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import requests, os
+
+app = Flask(__name__)
+TMDB_KEY = os.environ.get("TMDB_KEY")
+
+@app.route("/popular")
+def popular():
+    res = requests.get(
+        "https://api.themoviedb.org/3/movie/popular",
+        headers={"Authorization": f"Bearer {TMDB_KEY}"}
+    )
+    return jsonify(res.json())
+
+@app.route("/")
+def home():
+    return "TMDB backend working!"
